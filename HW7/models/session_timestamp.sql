188d212d8480d4c8d8c94195ecbf1ef190ff29b2
sneha-tumkur-narendra <sneha.tumkurnarendra@sjsu.edu>
2025-11-02 20:41:38 -0800

    Delete HW7/models/session_timestamp.sql

--- HW7/models/session_timestamp.sql
+++
@@ -1,9 +0,0 @@
-WITH __dbt__cte__session_timestamp AS (
-    SELECT
-     sessionId,
-     ts
-    FROM {{ source('raw', 'session_timestamp') }}
-    WHERE sessionId IS NOT NULL
-)
-
-SELECT * FROM __dbt__cte__session_timestamp
